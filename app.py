1ca114abba730fab681a3a2d181a41525eff576b
Anthony Radose <asradose@gmail.com>
2024-03-06 15:43:47 +0000

    Add initial functionality for sign up form

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, flash, render_template, redirect, request, session, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
     
@@ -21,6 +22,22 @@ def home():
 
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("sign_up"))
+
+        sign_up = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(sign_up)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
     return render_template("sign_up.html")
 
 @app.route("/get_games")
