a0b2368e90a773fd84d629847c4a94e213127033
Anthony Radose <asradose@gmail.com>
2024-04-03 00:53:40 +0000

    Disable DEBUG mode for production

--- app.py
+++ app.py
@@ -374,4 +374,4 @@ def delete_account():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)  # Make false when deployed!
+            debug=False)
